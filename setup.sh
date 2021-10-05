d62d31400a0dbb616a493254c8e52ced89e0991c
Neeokeaeng <86215141+Neeokeaeng@users.noreply.github.com>
2021-10-05 15:03:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 mkdir -p ~/.streamlit/
 
-echo "[theme]
-primaryColor = ‘#f5e6c4’
-backgroundColor = ‘#F8F9E0’ 
-secondaryBackgroundColor = ‘#E4E4E7’
-textColor= ‘#3C3C3C’
-font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[theme]\n\
+primaryColor = ‘#f5e6c4’\n\
+backgroundColor = ‘#F8F9E0’\n\ 
+secondaryBackgroundColor = ‘#E4E4E7’\n\
+textColor= ‘#3C3C3C’\n\
+font = ‘sans serif’\n\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
