3f32ef7269a8a1980af53d1bda408285bb123eda
Neeokeaeng <86215141+Neeokeaeng@users.noreply.github.com>
2021-10-05 15:05:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]\n\
-primaryColor = ‘#f5e6c4’\n\
-backgroundColor = ‘#F8F9E0’\n\ 
-secondaryBackgroundColor = ‘#E4E4E7’\n\
-textColor= ‘#3C3C3C’\n\
-font = ‘sans serif’\n\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
